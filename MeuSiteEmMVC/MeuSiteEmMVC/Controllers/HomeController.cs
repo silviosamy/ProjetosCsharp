b461738fc7618c7dd4b99bb8cdc900c8c8664265
Silvio Samy <silviosamysilva@hotmail.com>
2025-12-02 17:44:11 -0300

    Criada a conexão com o banco e criado método de criar na página de Usuário

--- MeuSiteEmMVC/MeuSiteEmMVC/Controllers/HomeController.cs
+++ MeuSiteEmMVC/MeuSiteEmMVC/Controllers/HomeController.cs
@@ -17,19 +17,18 @@ namespace MeuSiteEmMVC.Controllers
         {
             return View();
         }
-<<<<<<< HEAD
+
         public IActionResult User()
         {
             return View();
         }
         public IActionResult Products()
-=======
-
-        public IActionResult Privacy()
->>>>>>> 3df0da39a5325290ed31a0ea8e582449fed8c637
         {
             return View();
         }
+
+
+    
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
